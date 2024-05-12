e39f015fbdd5a0e000af49f538f1d81841b05614
dionysus <2803988963@qq.com>
2024-05-12 23:49:04 +0800

    添加:应用 package.json 导包

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios';
 
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import * as echarts from "echarts";
+
+Vue.prototype.$echarts = echarts
 Vue.config.productionTip = false
+Vue.use(ElementUI);
+Vue.prototype.$axios = axios
+Vue.use(router);
 
 new Vue({
   router,
